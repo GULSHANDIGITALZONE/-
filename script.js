af9e6d3f1374debfb4d490ad2cc109ce113a02d5
GDZ <154034148+GULSHANDIGITALZONE@users.noreply.github.com>
2025-08-04 12:47:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// <!-- //About ko click karne par modal open ho jayega. script........................................... -->
-
-function showAbout() {
-    document.getElementById('aboutModal').style.display = "block";
-}
-function closeAbout() {
-    document.getElementById('aboutModal').style.display = "none";
-}
